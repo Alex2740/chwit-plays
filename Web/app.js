afaff61e98d4ae604bddedc0bca1fdbf0c6a3119
Guillaume-Polverelli <75666966+Guillaume-Polverelli@users.noreply.github.com>
2023-06-13 20:47:33 +0200

    Ajout de l'app web pour requêtes http (input)

---
+++ Web/app.js
@@ -0,0 +1,24 @@
+
+function getValue() {
+    // Sélectionner l'élément input et récupérer sa valeur
+    var input = document.getElementById("in").value;
+    alert(input);
+
+    let url = "http://localhost:5000/inputs/0/";
+    url = url.concat(input);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.send();
+    xhr.responseType = "json";
+    xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        const data = xhr.response;
+        console.log(data);
+    } else {
+        console.log(`Error: ${xhr.status}`);
+    }
+    };
+}
+
+
