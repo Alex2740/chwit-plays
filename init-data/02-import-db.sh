5d708de85c26957178033819ee68a1e2e45b2c1f
BOUGUET Theo <theo.bougue@gmail.com>
2023-06-14 14:16:59 -0400

    Initialisation influxDB + correction script sh

--- init-data/02-import-db.sh
+++ init-data/02-import-db.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 echo "########### Loading data to Mongo DB ###########"
-mongoimport --jsonArray --db openGL --collection scene --file /tmp/data/init_scene.json
-mongoimport --jsonArray --db openGL --collection camera --file /tmp/data/init_camera.json
+mongoimport --jsonArray --db openGL --collection scene --file ./data/init_scene.json
+mongoimport --jsonArray --db openGL --collection camera --file ./data/init_camera.json
